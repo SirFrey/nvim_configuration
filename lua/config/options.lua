476d82bdcdcb8449e4c10646fd85c9a026156af4
SirFrey <moisescastellano12312@gmail.com>
2024-03-03 20:50:28 -0400

    fixed compatibilities's issues

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,11 +6,6 @@ vim.g.autoformat = false
 -- See `:help vim.o`
 vim.o.hlsearch = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
-vim.opt.undofile = true
-
 vim.opt.scrolloff = 8
 vim.opt.isfname:append '@-@'
 
