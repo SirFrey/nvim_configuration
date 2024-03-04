0adcf8a495c65353b5276d23bc696ac8cfa73c43
Moises <moisescastellano12312@gmail.com>
2024-03-03 22:23:12 -0400

    lualine cleaner

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,16 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function ()
-    require('lualine').setup()
-  end
+  config = function()
+    require('lualine').setup {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { '', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { '', '', '' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
+  end,
 }
