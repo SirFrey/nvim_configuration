ffb1620c2a8430430bc2bd804b26129f7ab9d8dc
Moises <moisescastellano12312@gmail.com>
2024-03-12 09:46:51 -0400

    reduced dependecies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -98,7 +98,7 @@ return {
         }
       end)
       require('mason-lspconfig').setup {
-        ensure_installed = {},
+        ensure_installed = { 'vimls', 'astro', 'clangd', 'lua_ls', 'tailwindcss', 'cssls', 'emmet_language_server', 'cssmodules_ls', 'html', 'tsserver' },
         handlers = {
           lsp_zero.default_setup,
           lua_ls = function()
